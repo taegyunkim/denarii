5c739dbbd86aebfa08a24805a072ca4f994e7aa1
Taegyun Kim <k.taegyun@gmail.com>
2019-12-17 14:35:33 -0500

    Initial simulator implementation.  (#8)
    * Use Clap to parse args.
    
    * Initial structure for simulator.
    
    * Simple argument parsing for ticks.
    
    * Packet arrival with Bernoulli trials.
    
    * Simple packet implementation (in-progress.)
    
    * Constructor for Packet.
    
    * Generate packet service time and resource requests randomly.
    
    * Implement step function for the packet.
    
    * Implement Algorithm trait for ceei and drf.
    
    * Implement PartialEq, Eq, and Hash for Packet.
    
    * Simulation outline.
    
    * Fix function headers.
    
    * Fix clippy warnings.
    
    * Add test for Packet struct.
    
    * Initial implementation of simulator routine.
    
    * Update comments.
    
    * Simplify step function.
    
    * Remove unused trait implementations.
    
    * Factor out run_allocation function.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,132 @@
-extern crate rand;
 mod algorithms;
 mod gurobi;
+mod simulator;
+
+extern crate clap;
+
+use algorithms::Algorithm;
+use clap::{App, Arg};
+use rand::distributions::{Bernoulli, Distribution};
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
-use std::env;
+use simulator::Packet;
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let seed: u64 = args[1].parse::<u64>().unwrap();
-    let num_resources: u64 = args[2].parse::<u64>().unwrap();
+    let matches = App::new("Denarii")
+        .version("0.1.0")
+        .author("Taegyun Kim <k.taegyun@gmail.com>")
+        .about("Discrete time based simulator for resource allocation in packet based network ddevices.")
+        .arg(
+            Arg::with_name("ticks")
+                .short("t")
+                .long("ticks")
+                .default_value("100")
+                .help("The number of ticks to run the simulator."),
+        )
+        .arg(
+            Arg::with_name("seed")
+                .short("s")
+                .long("seed")
+                .default_value("1")
+                .help("Random seed"),
+        )
+        .get_matches();
+
+    let seed: u64 = matches.value_of("seed").unwrap().parse::<u64>().unwrap();
     let mut rng: StdRng = SeedableRng::seed_from_u64(seed);
-    for x in 0..100 {
-        // Generate random demand vectors
-        let mut vec: Vec<i16> = Vec::new();
-        for _y in 0..num_resources {
-            vec.push(rng.gen_range(0, 1000));
+
+    let ticks = matches.value_of("ticks").unwrap().parse::<u64>().unwrap();
+
+    // Packets not allocated
+    let mut pkts: Vec<Packet> = Vec::new();
+    let mut completed: Vec<Packet> = Vec::new();
+
+    let p = 0.3;
+    // Distribution for packet arrivals.
+    let a_dist = Bernoulli::new(p).unwrap();
+    let num_resources = 2;
+
+    // TODO: Provide ways to set initial capacity of resources, 1) commandline
+    // argument, 2) randomly generate them.
+    let capacity: Vec<f64> = (0..num_resources)
+        .map(|x| ((x + 1) as f64) * 10.0)
+        .collect();
+
+    let alg = algorithms::Drf {};
+
+    let mut num_pkts = 0;
+    for t in 0..ticks {
+        let add_new_packet: bool = a_dist.sample(&mut rng);
+        // New Packet coming
+        if add_new_packet {
+            let service_time = rng.gen_range(10, 20) as f64;
+            let resource_req: Vec<f64> = (0..num_resources)
+                .map(|_| rng.gen_range(1, 11) as f64)
+                .collect();
+            println!(
+                "t:{}, service_time:{}, resource_req:{:?}",
+                t, service_time, resource_req
+            );
+
+            let p: Packet = Packet::new(num_pkts, t, service_time, resource_req);
+            num_pkts += 1;
+            pkts.push(p);
         }
-        println!("Hello, world {}, random vector: {:?}!", x, vec);
+
+        // Step each packet.
+        let mut done_pkts = 0;
+        for pkt in &mut pkts {
+            let done = pkt.step();
+
+            if done {
+                // TODO: Move instead of copy.
+                completed.push(pkt.clone());
+                done_pkts += 1;
+            }
+        }
+
+        // Remove packets that are completed.
+        pkts.retain(|pkt| !pkt.is_completed());
+
+        // Check whether a new allocation needs to happen
+        if !pkts.is_empty() && (add_new_packet || done_pkts > 0) {
+            run_allocation(&mut pkts, t, &capacity, &alg);
+        }
+    }
+
+    for pkt in &completed {
+        println!("{:?}", pkt);
+    }
+
+    println!("{}: Total number of packets", num_pkts);
+}
+
+fn run_allocation(pkts: &mut [Packet], t: u64, capacity: &[f64], alg: &dyn Algorithm) {
+    let mut requests: Vec<Vec<f64>> = Vec::new();
+    for pkt in pkts.iter() {
+        requests.push(pkt.resource_req.clone());
+    }
+    println!(
+        "t: {}, capacity: {:?} requests: {:?}",
+        t, capacity, requests
+    );
+    let coeffs = alg.allocate(&capacity, &requests);
+    assert!(coeffs.len() == pkts.len());
+    for (i, pkt) in pkts.iter_mut().enumerate() {
+        let alloc = pkt.resource_req.iter().map(|x| x * coeffs[i]).collect();
+        pkt.allocate(alloc);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn conatiner_ops() {
+        let mut pkts: Vec<Packet> = Vec::new();
+        pkts.push(Packet::new(1, 1, 2.0, vec![1.0, 2.0]));
+        pkts.retain(|pkt| pkt.is_scheduled());
+        assert!(pkts.is_empty());
     }
 }
---
+++ src/simulator/packet.rs
@@ -0,0 +1,89 @@
+#[derive(Clone, Debug, Default)]
+pub struct Packet {
+    /// Packet ID
+    id: u64,
+    /// Time t, the packet showed up.
+    t_arrival: u64,
+    /// Time t, the packet left the hardware
+    t_departure: u64,
+    /// Resources requested
+    pub resource_req: Vec<f64>,
+    /// Resources actually allocated, empty if none.
+    resource_alloc: Vec<f64>,
+    /// Number of ticks it needs to complete, given requested resources.
+    service_time: f64,
+    /// Actual service time it has gotten so far.
+    adjusted_service_time: f64,
+}
+
+impl Packet {
+    pub fn new(id: u64, t_arrival: u64, service_time: f64, resource_req: Vec<f64>) -> Packet {
+        Packet {
+            id,
+            t_arrival,
+            resource_req,
+            service_time,
+            ..Default::default()
+        }
+    }
+
+    /// Steps one tick.
+    pub fn step(&mut self) -> bool {
+        if !self.is_scheduled() {
+            return false;
+        }
+
+        // Update adjusted service time by the fraction of resources it got.
+        let ratio = self.resource_alloc[0] / self.resource_req[0];
+        self.adjusted_service_time += ratio;
+
+        // Also update t_departure by 1.
+        self.t_departure += 1;
+
+        self.is_completed()
+    }
+
+    pub fn is_completed(&self) -> bool {
+        self.adjusted_service_time >= self.service_time
+    }
+
+    pub fn is_scheduled(&self) -> bool {
+        !self.resource_alloc.is_empty()
+    }
+
+    pub fn allocate(&mut self, alloc: Vec<f64>) {
+        self.resource_alloc = alloc;
+    }
+
+    #[allow(dead_code)]
+    /// Returns the number of ticks it actually took to service this packet.
+    /// Make sure you check whether this packet is completed, using
+    /// is_completed().
+    pub fn latency(&self) -> u64 {
+        self.t_departure - self.t_arrival
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple() {
+        let mut p = Packet::new(1, 3, 5.0, vec![2.0, 3.0]);
+        assert_eq!(p.id, 1);
+        assert_eq!(p.t_arrival, 3);
+        assert_eq!(p.service_time, 5.0);
+        assert_eq!(p.resource_req, [2.0, 3.0]);
+        assert_eq!(p.is_completed(), false);
+        assert_eq!(p.is_scheduled(), false);
+        p.allocate(vec![1.0, 1.5]);
+        assert!(p.is_scheduled());
+        // Service time is set to 5 and it was allocated with half of what it
+        // requested. So needs 5 * 2 ticks to complete.
+        for _ in 0..10 {
+            p.step();
+        }
+        assert!(p.is_completed());
+    }
+}
